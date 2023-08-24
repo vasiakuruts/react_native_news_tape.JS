7965d357683e155e1d871b09107835634c7119d8
vasiakuruts <77865213+vasiakuruts@users.noreply.github.com>
2023-08-24 21:36:12 +0300

    Create app pages
    - Add main pages
    - Add contackt pages

---
+++ src/components/g-styles/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const gStyle = StyleSheet.create({
+  main: {
+    flex: 1,
+    padding: 45,
+  },
+  title: {
+    fontSize: 20,
+    color: "#333",
+    fontFamily: "mt-light",
+  },
+});
