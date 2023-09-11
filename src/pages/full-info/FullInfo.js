1009a16091e12ba0b2c9ca86d3270b5cb80fe55a
vasiakuruts <77865213+vasiakuruts@users.noreply.github.com>
2023-09-11 23:19:03 +0300

    Add mobal screen
    - Add to icons mobal

--- src/pages/full-info/FullInfo.js
+++ src/pages/full-info/FullInfo.js
@@ -1,26 +1,18 @@
 import React from "react";
 import { Text, View, Image } from "react-native";
 import { gStyle } from "../../components/g-styles/style";
+import styles from "./styles";
 
 const FullInfoPage = ({ route }) => {
-  console.log("====================================");
-  console.log(route.params);
-  console.log("====================================");
   if (route.params === undefined) {
     return <Text style={gStyle.title}>Not data</Text>;
   } else {
     return (
       <View style={gStyle.main}>
-        <Image
-          source={{ uri: route.params.img }}
-          style={{
-            width: "100%",
-            height: 400,
-          }}
-        />
-        <Text style={gStyle.title}>{route.params.name}</Text>
-        <Text style={gStyle.title}>{route.params.anons}</Text>
-        <Text>{route.params.full}</Text>
+        <Image source={{ uri: route.params.img }} style={styles.image} />
+        <Text style={[gStyle.title, styles.title]}>{route.params.name}</Text>
+        <Text style={[gStyle.title, styles.anons]}>{route.params.anons}</Text>
+        <Text style={[gStyle.title, styles.anons]}>{route.params.full}</Text>
       </View>
     );
   }
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import {
   Text,
   View,
-  Button,
   FlatList,
   TouchableOpacity,
   Image,
+  Modal,
 } from "react-native";
 import { gStyle } from "../../components/g-styles/style";
 import styles from "./styles";
 import temporaryNews from "../../temporary";
+import { Ionicons } from "@expo/vector-icons";
 
 const MainPage = ({ navigation }) => {
   const loadScene = () => {
@@ -17,8 +18,28 @@ const MainPage = ({ navigation }) => {
   };
 
   const [news, setNews] = useState(temporaryNews);
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={gStyle.main}>
+      <Modal visible={modalVisible}>
+        <View style={gStyle.main}>
+          <Ionicons
+            name="close-circle"
+            size={30}
+            color="red"
+            onPress={() => setModalVisible(false)}
+            style={styles.iconClose}
+          />
+          <Text style={styles.title}>Форма для додаваннка новини</Text>
+        </View>
+      </Modal>
+      <Ionicons
+        name="add-circle"
+        size={30}
+        color="green"
+        onPress={() => setModalVisible(true)}
+        style={styles.iconAdd}
+      />
       <Text style={[gStyle.title, styles.headers]}>Голова сторінка</Text>
       <FlatList
         data={news}
@@ -27,13 +48,7 @@ const MainPage = ({ navigation }) => {
             style={styles.item}
             onPress={() => navigation.navigate("FullInfo", item)}
           >
-            <Image
-              source={{ uri: item.img }}
-              style={{
-                width: 100,
-                height: 100,
-              }}
-            />
+            <Image source={{ uri: item.img }} style={styles.image} />
             <Text style={styles.title}>{item.name}</Text>
           </TouchableOpacity>
         )}
